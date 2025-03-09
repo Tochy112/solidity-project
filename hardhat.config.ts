9c64fca04254e03ea90115c2df2d869354af2132
Ukwuoma Tochukwu <ukwuomatochi112@gmail.com>
2025-03-10 00:47:10 +0100

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ const config: HardhatUserConfig = {
   networks: {
     sepolia: {
       url: "https://wild-wild-asphalt.ethereum-sepolia.quiknode.pro/4d49c2ad72fe7b0c3fa2334c49dd40121ed1a520/",
-      accounts: ["021a47c200951e02c4bf5512378811727088149f06aaf33903f10a9171bba037"]
+      accounts: [""]
     }
   }
 };
